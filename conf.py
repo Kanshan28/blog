5d045e7f1b331e22dee16639ad229d9c43fc2a6e
Kanshan28 <1432018695@qq.com>
2020-04-29 16:26:42 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,14 +19,15 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "刘看山的个人博客"
 site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+#site_build_date = "2019-12-18T16:51+08:00"
+site_build_date = "2020-4-29T16:20+08:00"
+author = "刘看山"
+email = "kanshan_liu@foxmail.com"
+author_homepage = "https://www.liukanshan.club"
+description = "TALK IS CHEAP , SHOW ME CODE !"
+key_words = ['Maverick', '刘看山', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
