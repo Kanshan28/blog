9f3367f07fc2a5871a37df9096741e0b2ff6032c
Kanshan28 <46599889+Kanshan28@users.noreply.github.com>
2020-04-29 17:52:52 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,15 +19,15 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "刘看山的个人博客"
+site_name = "刘看山的博客"
 site_logo = "${static_prefix}logo.png"
 #site_build_date = "2019-12-18T16:51+08:00"
 site_build_date = "2020-04-29T17:40+08:00"
 author = "刘看山"
 email = "kansha_liu@foxmail.com"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+description = "TALK IS CHEAP , SHOW ME CODE !"
+key_words = ['Maverick', '刘看山', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
@@ -36,9 +36,9 @@ external_links = [
         "brief": "🏄‍ Go My Own Way."
     },
     {
-        "name": "三無計劃",
-        "url": "https://www.imalan.cn",
-        "brief": "熊猫小A的主页。"
+        "name": "刘看山",
+        "url": "https://www.liukanshan.club",
+        "brief": "刘看山的主页。"
     }
 ]
 nav = [
@@ -61,18 +61,13 @@ nav = [
 
 social_links = [
     {
-        "name": "Twitter",
-        "url": "https://twitter.com/AlanDecode",
-        "icon": "gi gi-twitter"
-    },
-    {
         "name": "GitHub",
-        "url": "https://github.com/AlanDecode",
+        "url": "https://github.com/Kanshan28",
         "icon": "gi gi-github"
     },
     {
         "name": "Weibo",
-        "url": "https://weibo.com/5245109677/",
+        "url": "https://weibo.com/5366743913/",
         "icon": "gi gi-weibo"
     }
 ]
