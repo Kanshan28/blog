37268c075d42be64c6d771b13aa4a25182189645
Kanshan28 <46599889+Kanshan28@users.noreply.github.com>
2020-04-29 16:43:51 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -21,8 +21,8 @@ enable_jsdelivr = {
 # 站点设置
 site_name = "刘看山的个人博客"
 site_logo = "${static_prefix}logo.png"
-#site_build_date = "2019-12-18T16:51+08:00"
-site_build_date = "2020-4-29T16:20+08:00"
+site_build_date = "2019-12-18T16:51+08:00"
+#site_build_date = "2020-4-29T16:20+08:00"
 author = "刘看山"
 email = "kanshan_liu@foxmail.com"
 author_homepage = "https://www.liukanshan.club"
