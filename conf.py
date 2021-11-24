b3e2d4c05824a282ec8d73f36fafe97320606796
Kanshan28 <46599889+Kanshan28@users.noreply.github.com>
2021-11-24 14:44:20 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -5,7 +5,7 @@
 # For Maverick
 #site_prefix = "/Blog-With-GitHub-Boilerplate/"
 #site_prefix = "/"
-site_prefix = "https://kanshan28.github.io/Blog"
+site_prefix = "/Blog"
 source_dir = "../src/"
 build_dir = "../dist/"
 index_page_size = 10
